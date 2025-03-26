045a0181f4aa7da8ffd8ae45b31021306a510a76
Maniik Ambara <maniikambarawan@gmail.com>
2025-03-26 14:44:50 +0800

    update

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
+}
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
